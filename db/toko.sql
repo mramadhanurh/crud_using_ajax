30a6812ef0d8795e8bf29b4524220c0128c62e25
mramadhanurh <mramadhanur@gmail.com>
2022-07-01 08:19:14 +0700

    Tambah Database

---
+++ db/toko.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 01 Jul 2022 pada 03.16
+-- Versi server: 10.4.20-MariaDB
+-- Versi PHP: 7.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toko`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_data`
+--
+
+CREATE TABLE `tbl_data` (
+  `id` int(11) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `harga_barang` int(7) NOT NULL,
+  `stok` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tbl_data`
+--
+
+INSERT INTO `tbl_data` (`id`, `nama_barang`, `harga_barang`, `stok`) VALUES
+(1, 'Sepatu', 250000, 50),
+(2, 'Baju', 100000, 25),
+(3, 'sepatu', 12000, 35);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_data`
+--
+ALTER TABLE `tbl_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_data`
+--
+ALTER TABLE `tbl_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
